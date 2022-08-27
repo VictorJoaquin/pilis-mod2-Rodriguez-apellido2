aade4b847c31528ee0f91929e7948cd2bf915f95
Joaquín Rodriguez <44090456+VictorJoaquin@users.noreply.github.com>
2022-08-27 00:31:12 -0300

    api clima

--- js/main.js
+++ js/main.js
@@ -29,6 +29,30 @@ boton.addEventListener('click',function(){
                     'success'
                 );
             })
+
     }else
         console.log("No se ingresaron datos")
 })
+
+
+//consulta a Api del clima
+//https://api.openweathermap.org/data/2.5/weather?id=3836564&appid=057bee1d4b18ab2fe4b0394804bd577d
+
+fetch ("https://api.openweathermap.org/data/2.5/weather?id=3836564&appid=057bee1d4b18ab2fe4b0394804bd577d")
+.then(response => response.json())
+.then(data => { 
+    mostrarClima(data)
+
+    console.log(data.main.temp)
+    
+})
+.catch(err => console.log(err))
+function mostrarClima(data){
+    let ciudad = data.name +"             ";
+    let temperatura = "Temperatura "+ ((data.main.temp - 273.15).toFixed(0)) + "ºC";
+    let humedad = "  Humedad: " + data.main.humidity + "%" ;
+
+    document.getElementById("clima1").innerHTML = ciudad;
+    document.getElementById("clima2").innerHTML = temperatura + humedad;
+
+}
